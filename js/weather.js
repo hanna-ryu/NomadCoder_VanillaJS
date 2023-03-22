6ff8c4b4cb4282cff371d381948dfc74b0e262cb
HannaRyu <foureaf@gmail.com>
2023-03-22 22:50:07 +0900

    add weather

---
+++ js/weather.js
@@ -0,0 +1,21 @@
+function onGeoOk(position){
+    const lat = position.coords.latitude;
+    const lng = position.coords.longitude;
+    const API_KEY = "1bef3c9ca46b83d59cdb00830dad11f8";
+    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&appid=${API_KEY}&units=metric`
+    fetch(url)
+        .then(response => response.json())
+        .then(data => {
+            const city =  document.querySelector("#weather span:first-child");
+            const weather = document.querySelector("#weather span:last-child");
+            city.innerText  = data.name;
+            weather.innerText =  data.weather[0].main;
+        });
+
+};
+
+function onGeoError(){
+    alert("Can't find you :(")
+}
+
+navigator.geolocation.getCurrentPosition(onGeoOk,onGeoError)
